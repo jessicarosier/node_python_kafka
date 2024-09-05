a66b273ca1fbaa0dc7a737c53823663fad8038dd
Jessica Rosier <jessica.c.rosier1@gmail.com>
2024-09-05 15:00:54 -0500

    final commit for day

--- scripts/run_consumer.sh
+++ scripts/run_consumer.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Set the path to your Python script
 SCRIPT_PATH="/Users/jessicarosier/IdeaProjects/svelte-projects/node_python_kafka/python/consumer_middleware.py"
 
-# Set the path to your virtual environment
 VENV_PATH="/Users/jessicarosier/IdeaProjects/svelte-projects/node_python_kafka/venv"
 
-# Function to start the consumer
 start_consumer() {
     echo "Starting Kafka consumer..."
     source "$VENV_PATH/bin/activate"
     python "$SCRIPT_PATH"
 }
-
 
 
 start_consumer
